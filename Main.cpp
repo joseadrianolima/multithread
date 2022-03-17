8932a3e634fdbd5feaa5bc3ef4c7d19e8705b094
joseadrianolima <adriano.lima.dk@gmail.com>
2022-03-17 07:13:18 -0300

    First commit

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   char arr[11] = "0123456789";
 
-  std::cout << "Teste" << " " << arr[5];
+  std::cout << "Teste"
+            << " " << arr[5];
 
-  for (int i=1; i<=5; i++) {
+  for (int i = 1; i <= 5; i++)
+  {
     std::cout << "Valor " << i << "\n";
   }
   return 0;
